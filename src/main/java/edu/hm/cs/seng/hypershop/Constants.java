aa4b020d8b70b656d96413470db5ebb93216b8a2
Andreas Kramer <akramer@kolb-digital.de>
2018-12-03 18:32:57 +0100

    Fixed merge

--- src/main/java/edu/hm/cs/seng/hypershop/Constants.java
+++ src/main/java/edu/hm/cs/seng/hypershop/Constants.java
@@ -3,6 +3,8 @@ package edu.hm.cs.seng.hypershop;
 public class Constants {
 
 
+
+
     private Constants() {
         // disable public constructor
     }
@@ -17,6 +19,7 @@ public class Constants {
     public static final String INTENT_BACK = "BackIntent";
     public static final String INTENT_EDIT_RECIPE = "EditRecipeIntent";
     public static final String INTENT_REMOVE_RECIPE = "RemoveRecipeIntent";
+    public static final String INTENT_LIST_RECIPES = "ListRecipesIntent";
 
     public static final String SLOT_AMOUNT = "amount";
     public static final String SLOT_UNIT = "unit";
